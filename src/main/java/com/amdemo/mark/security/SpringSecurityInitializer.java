63a5e4365e9b6281234b280267aa039e1479ed85
skiwheelr <skiwheelr@gmail.com>
2020-06-05 11:29:39 -0700

    authentication disabled

---
+++ src/main/java/com/amdemo/mark/security/SpringSecurityInitializer.java
@@ -0,0 +1,7 @@
+//package com.amdemo.mark.security;
+//
+//import org.springframework.security.web.context.AbstractSecurityWebApplicationInitializer;
+//
+//public class SpringSecurityInitializer extends AbstractSecurityWebApplicationInitializer {
+////no code needed
+//}
